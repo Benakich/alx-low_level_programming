957508ce8d82b3664a693a68a0a01d20cf333b0c
Benakich <Benakich@gmail.com>
2022-09-09 00:58:50 +0000

    debugging

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Entry point main
+ *
+ * Returns 0: Always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
