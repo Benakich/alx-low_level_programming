f9a7927e0f3180c1bddf6c5c122039378f9bc6ec
Benakich <Benakich@gmail.com>
2022-09-09 20:52:55 +0000

    debugging

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ int main(void)
 	}
 	else if (last_num == 0)
 	{
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
