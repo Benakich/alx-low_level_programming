79be5f182223457deb31e01483c6dc812a0b00d0
Benakich <Benakich@gmail.com>
2022-09-09 23:49:40 +0000

    print hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,3 +6,17 @@
  */
 int main(void)
 {
+	int a = 0;
+	char c = 'a';
+
+	for (; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
