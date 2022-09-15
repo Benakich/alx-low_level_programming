90c0f0e53857f078e7efc26e57bd626506bd876e
Benakich <Benakich@gmail.com>
2022-09-15 02:04:45 +0000

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,24 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = (i * j);
-			if (k >= 10)
+			if (j != 0) 
 			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
-			else
+			if (j == 0)
 			{
-				_putchar(k + '0');
-				_putchar(',');
+				_putchar('0');
+                        }
+			else if (k >= 10)
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			else if (k < 10 && j != 0)
+			{
 				_putchar(' ');
-				_putchar(' ');
+				_putchar((k % 10)  + '0');
 			}
 		}
 		_putchar('\n');
