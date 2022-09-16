ea542398e6460bdf8750d2a6d1a5eb29672bf757
Benakich <Benakich@gmail.com>
2022-09-16 17:58:30 +0000

    added a putchar for space rather than in printf

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print numbers 1-100, print fizz for multiples of 3 and Buzz for
- * multiples of 5.
+ * main - print numbers 1-100, 
+ * print fizz for multiples of 3 
+ * Buzz for multiples of 5.
  * print fizzBuzz for multiples of 3 and 5
  *
  * Return: 0 Always
@@ -13,15 +14,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i != 0 && (i % 3) == 0 && (i % 5) != 0)
-			printf("Fizz ");
+		if ((i % 3) == 0 && (i % 5) != 0)
+			printf("Fizz");
 		else if ((i % 3) != 0 && (i % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 
-		else if (i != 0 && (i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
