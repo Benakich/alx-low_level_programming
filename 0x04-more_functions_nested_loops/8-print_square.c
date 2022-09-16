1828c5ca380c2ec1dee92fd46a95777196878692
Benakich <Benakich@gmail.com>
2022-09-16 17:24:14 +0000

    fizz_buzz

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,7 +6,7 @@
  */
 void print_square(int size)
 {
-	int i,j;
+	int i, j;
 
 	if (size <= 0)
 	{
@@ -14,7 +14,7 @@ void print_square(int size)
 	}
 	for (i = 1; i <= size; i++)
 	{
-		for (j =1; j <= size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
 		}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - print numbers 1-100, print fizz for multiples of 3 and Buzz for
+ * multiples of 5.
+ * print fizzBuzz for multiples of 3 and 5
+ */
+void fizz_buzz(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i != 0 && (i % 3) == 0 && (i % 5) != 0)
+			printf("Fizz ");
+		else if ((i % 3) != 0 && (i % 5) == 0)
+			printf("Buzz ");
+
+		else if (i != 0 && (i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+
+	}
+}
