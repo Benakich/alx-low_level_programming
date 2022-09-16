aef20714a69df09c23cca4d607e5b0846acc4ef6
Benakich <Benakich@gmail.com>
2022-09-16 17:02:05 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: number of elements in square
+ */
+void print_square(int size)
+{
+	int i,j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j =1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
