e00f33f90f5ed886d8e9a36445923460f28c116a
Benakich <Benakich@gmail.com>
2022-09-16 21:43:45 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - print diagonal line
+ * @size: number of lines to print
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= (size - i) ; j++)
+			_putchar(' ');
+		for (k = 1; k <= i; k++)
+			_putchar('#');
+		_putchar('\n');
+
+	}
+}
