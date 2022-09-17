b32e2cfefd0fd813913f0e71fb0b8231c439dc3b
Benakich <Benakich@gmail.com>
2022-09-17 00:50:06 +0000

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,15 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
+	else if (b > c)
 	{
 		largest = b;
 	}
