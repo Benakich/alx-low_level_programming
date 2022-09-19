89502b69392c0bb68f173f7d4b5e25b73d021bad
Benakich <Benakich@gmail.com>
2022-09-19 23:02:03 +0000

    trying to restore git push, after getting fatal error

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - fn to reset a paramter nad updates it values to 98
+ *
+ * @n: pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Script 0 - update an entry using a pointer
+Script 1 -
