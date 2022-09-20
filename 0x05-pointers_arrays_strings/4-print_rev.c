1ff0a27cdcf680e74fd4e5b9c600fe6ca634c453
Benakich <Benakich@gmail.com>
2022-09-20 01:00:30 +0000

    print revers e

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string followed by a newe line to stdout
+ *
+ * @str: string to be printed
+ */
+void print_rev(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0';)
+	{
+		i++;
+	}
+	i--;
+	for (i--; i >= 0; i--)
+	{
+		_putchar(str[i--]);
+	}
+	_putchar('\n');
+}
