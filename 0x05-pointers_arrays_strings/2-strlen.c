a27d80b52397661ab986af8a43342c4784295546
Benakich <Benakich@gmail.com>
2022-09-20 00:07:39 +0000

    counting characters

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *_strlen - len of a given string
+ *
+ * @s: len to be counted
+ *
+ * Return: strlen
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+	 
+	while (*s != '\0')
+	{
+		a++;
+		s++;
+	}
+
+	return (a);
+}
+
