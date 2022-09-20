c73e44a07ec059e17ba76ac46b7d1f8aac4d2fdb
Benakich <Benakich@gmail.com>
2022-09-20 16:02:42 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - fn to print n elements of an array of integers
+ *
+ * @a: pointer type int
+ * @n: integer
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	_putchar('\n');
+}
