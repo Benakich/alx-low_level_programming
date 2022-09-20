5fc60d8c33f3920c19b8283ea2043fd592ddc018
Benakich <Benakich@gmail.com>
2022-09-20 16:34:56 +0000

    debug

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,12 @@ void puts_half(char *str)
 	{
 		i++;
 	}
-	if (i % 2 != 0)
-		j = (i / 2);
-	else
+	if (i % 2 == 1)
 		j = (i - 1) / 2;
-	for (; j <= i; j++)
+		j+= 1;
+	else
+		j = i / 2;
+	for (; j < i; j++)
 	{
 		_putchar(str[j]);
 	}
