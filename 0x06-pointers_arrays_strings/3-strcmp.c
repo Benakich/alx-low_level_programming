929dfecc2b876f553f5edea2e99e8db0e7fb7f08
Benakich <Benakich@gmail.com>
2022-09-22 03:16:33 +0000

    fn to compare two strings and return int

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int j;
-
-	j = s1[0] - s2[0];
-
-	return (j);
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
 }
