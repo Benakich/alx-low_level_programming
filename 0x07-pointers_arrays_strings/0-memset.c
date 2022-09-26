c1327966be4ae6c22f0289127403b51a7369198e
Benakich <Benakich@gmail.com>
2022-09-26 21:26:12 +0000

    fn that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,20 +5,17 @@
  * @s: pointer to memorey
  * @b: constant byte b to fill the n bytes of the memory area pointed to by s
  * @n: unsigned int n, no of bytes to be filled
- * 
- * Return - Char
+ *
+ * Return: char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
 
-	i = 0;
 	while (n > 0)
 	{
-		s[i] = b;
-		i++;
+		*s = b;
 		n--;
-		
+		s++;
 	}
 	return (s);
 }
