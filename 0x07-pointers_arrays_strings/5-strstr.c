98c62c1b926e97eed56fefeeb5f2ca4b2c484d93
Benakich <Benakich@gmail.com>
2022-09-27 01:01:50 +0000

    needle in haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+/**
+* *_strstr - fn that locates a substring
+*
+* @haystack: string pointer type char
+* @needle: string pointer type char
+
+* Return: char
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+
+			} while (haystack[i] == needle[i]);
+					}
+		haystack++;
+	}
+
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ char *_strchr(char *s, char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif /* MAIN_H */
