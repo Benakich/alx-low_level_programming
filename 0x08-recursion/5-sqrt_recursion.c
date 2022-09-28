453c0dae4706d656e8a181a36061f2a79a223be4
Benakich <Benakich@gmail.com>
2022-09-28 23:58:37 +0000

    recursion is fun

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+int _sqrt_recursion(int n);
+int _find_sqrt(int n, unsigned int i);
+
+/**
+* _find_sqrt - fn to find square root
+* @n: int to find sqrt of
+* @i: divisor
+*
+* Return: i if sqrt found, otherwise -1
+*/
+
+int _find_sqrt(int n, unsigned int i)
+{
+	while (i > 0)
+	{
+		if (i * i == n)
+			return (i);
+		else
+			return (_find_sqrt(n, (i - 1)));
+	}
+	return (-1);
+}
+
+
+
+/**
+* _sqrt_recursion - fn to find natural square root
+* @n: number to find natural square root of
+*
+* Return: if n does not have aa nat sqr_root -1, else int
+*/
+
+int _sqrt_recursion(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (n);
+	i = n / 2;
+
+	return (_find_sqrt(n, i));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+int find_prime(int n, int i);
+int is_prime_number(int n);
+
+/**
+* find_prime - fn to find if a number is a prime number
+* @n: int number, check if prime
+* @i: int number, divisor
+*
+* Return: 1 if prime, 0 otherwise
+*/
+
+int find_prime(int n, int i)
+{
+	while (i > 1)
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (find_prime(n, (i - 1)));
+
+	}
+	return (1);
+}
+
+/**
+* is_prime_number - fn to find if a number is prime
+* @n : int, number to check prime
+*
+* Return: 1 if prime, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+	unsigned int i;
+
+	if (n <= 1)
+		return (0);
+	if (n == 2 || n == 3 || n == 5 || n == 7)
+		return (1);
+
+	i = n / 2;
+
+	return (find_prime(n, i));
+}
