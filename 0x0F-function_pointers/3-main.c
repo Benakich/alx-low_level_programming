6898dbf2773e8c69b622e3153c3108b601a2ff4c
Benakich <Benakich@gmail.com>
2022-10-13 03:23:57 +0000

    updated the gitignore to allow *.c push

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	char *s = argv[2];
+	char *s;
 	int num1;
 	int num2;
 
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	s = argv[2];
 
 	if (*s == '/' && num2 == 0)
 	{
