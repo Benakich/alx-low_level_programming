c5b871d08c5f79a8cc2fa6df47bf028b3174ff71
Benakich <Benakich@gmail.com>
2022-10-13 03:18:48 +0000

    updated the gitignore to allow *.c push

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arguement count
+ * @argv: argument value
+ *
+ * Return: Always 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s = argv[2];
+	int num1;
+	int num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (*s == '/' && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (get_op_func(s) == NULL || s[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+
+	}
+	printf("%d\n", get_op_func(s)(num1, num2));
+
+	return (0);
+}
