1873e59f72098577e3905784c1a16a7e80e59be2
Benakich <Benakich@gmail.com>
2022-10-13 23:52:08 +0000

    fn to print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - fn that returns the sum of all its parameters
+ * @separator: char str to separate numbers
+ * @n: int n to be printed
+ * @...: variable number of argumenets
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list c;
+	char *hold;
+	unsigned int i;
+
+	va_start(c, n);
+	for (i = 0; i < n; i++)
+	{
+		hold = va_arg(c, char *);
+		if (hold == NULL)
+				printf("(nil");
+		else
+			printf("%s", hold);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(c);
+}
