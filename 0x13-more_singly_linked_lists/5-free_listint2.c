91fcd9050d22677a6f7fd3aeb0c74ba38f295e5c
Benakich <Benakich@gmail.com>
2022-10-25 00:53:07 +0000

    free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - fn to free list, sets head to NULL
+ * @head: list head pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *del;
+
+	while (*head)
+	{
+		del = *head;
+		*head = (*head)->next;
+		free(del);
+	}
+	head = NULL;
+}
