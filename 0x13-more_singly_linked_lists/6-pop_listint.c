433ce8b1dc0c298fa468113495596d070f04ae9a
Benakich <Benakich@gmail.com>
2022-10-25 01:04:37 +0000

    delete header node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,23 @@
-#include <stlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * pop_listint - fn to delete the head node of a list
+ * @head: pointer to head node
+ * Return: return head node data n
+ */
 
+int pop_listint(listint_t **head)
+{
+	listint_t *del;
+	int m;
+
+	del = (*head)->next;
+	m = (*head)->n;
+	free(*head);
+
+	*head = del;
+	return (m);
+}
 
