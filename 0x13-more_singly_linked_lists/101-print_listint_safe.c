2a349de878f73050f98d81301d58acf276edf18d
Benakich <Benakich@gmail.com>
2022-10-27 00:44:28 +0000

    fn that prints a listint_t list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,32 @@
 #include <stdlib.h>
 
 /**
+ * find_node_loop - finds a loop in a linked list
+ * @head: head pointer
+ * Return: address of noder where loop start
+ */
+
+listint_t *find_node_loop(listint_t *head)
+{
+	listint_t *ptr, *ptr2;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (ptr2 = head->next; ptr2 != NULL;  ptr2 = ptr2->next)
+	{
+		if (ptr2 == ptr2->next)
+			return (ptr2);
+		for (ptr = head; ptr != ptr2; ptr = ptr->next)
+			if (ptr == ptr2->next)
+				return (ptr2->next);
+	}
+	return (NULL);
+
+}
+
+
+/**
  * print_listint_safe - fn to print a linked list
  * @head: head pointer
  * Return: the number of nodes in the list
@@ -10,27 +36,24 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *ptr, *current;
 	size_t count;
+	int loop;
+	listint_t *node_loop;
 
-	current = head;
-	if (current == NULL)
-		exit(98);
+	node_loop = find_node_loop((listint_t *) head);
 
-	count = 0;
-	while (current != NULL)
+	for (count = 0, loop = 1; (head != node_loop || loop) && head != NULL;)
 	{
-		ptr = current;
-		current = current->next;
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (head == node_loop)
+			loop = 0;
+		head = head->next;
 		count++;
-		printf("[%p] %d\n", (void *) ptr, ptr->n);
-
-		if (ptr < current)
-		{
-			printf("-> [%p] %d\n", (void *) current, current->n);
-			break;
-		}
 	}
 
+	if (node_loop != NULL)
+		printf("-> [%p] %i\n", (void *) head, head->n);
+
 	return (count);
+
 }
