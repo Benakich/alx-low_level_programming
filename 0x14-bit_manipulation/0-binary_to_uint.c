16493615a03db2d21bcccff32dbd4aa36f657cb5
Benakich <Benakich@gmail.com>
2022-10-27 23:53:08 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * binary_to_uint - fn that converts a binary number to an unsigned int
+ * @b: pointer to binary number
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int counter = 0;
+	unsigned int m = 0;
+	unsigned int exp = 1;
+
+	
+	if (b == NULL)
+		return (0);
+
+	for (;b[counter];)
+	{
+		if (b[counter] != '0' && b[counter] != '1')
+			return (0);
+		counter++;
+	}
+
+	for (counter -= 1; counter >= 0; counter--)
+	{
+		m += ((b[counter] - '0') * exp);
+		exp *= 2;
+	}
+
+	return (m);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,7 @@
+Script 0 - fn that converts a binary number to an unsigned int
+Script 1 - fn that prints the binary rep of a number
+Script 2 - fn that returns the value of a bit at a given index
+Script 3 - fn that sets the value of a bit to 1 at a given index
+Script 4 - fn that sets the value of a bit to 0 at a given index
+Script 5 - fn that returns the number of bits you would need to flip to get from one number to another
+Script 6 - 
