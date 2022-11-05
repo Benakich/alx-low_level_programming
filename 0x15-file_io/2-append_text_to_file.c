6a6d78aef120915736139ee467c6ba787b716473
Benakich <Benakich@gmail.com>
2022-11-05 00:04:40 +0000

    file append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 
 	if (text_content != NULL)
 	{
